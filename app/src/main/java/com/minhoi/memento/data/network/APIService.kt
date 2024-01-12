e20bf8ca1a9a5f4be2f8e9d45af959a542ed6707
MinhoiKoo <minlove2013@gmail.com>
2024-01-12 21:06:35 +0900

    Feat: Retrofit2 객체, API 생성

---
+++ app/src/main/java/com/minhoi/memento/data/network/APIService.kt
@@ -0,0 +1,18 @@
+package com.minhoi.memento.data.network
+
+import com.minhoi.memento.data.dto.MajorDto
+import com.minhoi.memento.data.dto.SchoolDto
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface APIService {
+
+    @GET("/api/schools")
+    suspend fun getSchools(): Response<List<SchoolDto>>
+
+    @GET("/api/{schoolName}")
+    suspend fun getMajors(
+        @Path(value = "schoolName") schoolName: String
+    ): Response<List<MajorDto>>
+}
