9fd805b8c250c0e880c9a25a7e84e233918c79c4
MinhoiKoo <minlove2013@gmail.com>
2024-01-13 21:58:31 +0900

    feat: 전공 목록 RecyclerView ItemDecoration 추가
    - 구분선, 여백 추가

--- app/src/main/java/com/minhoi/memento/ui/join/SchoolMajorFragment.kt
+++ app/src/main/java/com/minhoi/memento/ui/join/SchoolMajorFragment.kt
@@ -1,13 +1,17 @@
 package com.minhoi.memento.ui.join
 
+import android.icu.lang.UCharacter.VerticalOrientation
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.minhoi.memento.MajorAdapter
+import com.minhoi.memento.MajorItemDecoration
 import com.minhoi.memento.databinding.FragmentSchoolMajorBinding
 
 class SchoolMajorFragment : Fragment() {
@@ -32,17 +36,19 @@ class SchoolMajorFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        majorAdapter = MajorAdapter()
+
+        majorAdapter = MajorAdapter() {}
         binding.majorRv.apply {
             adapter = majorAdapter
             layoutManager = LinearLayoutManager(requireActivity())
+            addItemDecoration(MajorItemDecoration(80))
         }
         setObserve()
     }
 
     private fun setObserve() {
         joinViewModel.majors.observe(viewLifecycleOwner) { majors ->
-            majorAdapter.setItems(majors.map{it.name})
+            majorAdapter.setItems(majors)
         }
     }
 
