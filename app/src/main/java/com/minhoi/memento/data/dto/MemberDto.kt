dd0a03f43e5d3c3eed28a7ce2cda75a8caf27fb7
MinhoiKoo <minlove2013@gmail.com>
2024-01-18 16:38:22 +0900

    feat: MemberDto 구현
    - 회원가입시 서버에 보내는 객체

---
+++ app/src/main/java/com/minhoi/memento/data/dto/MemberDto.kt
@@ -0,0 +1,11 @@
+package com.minhoi.memento.data.dto
+
+data class MemberDto(
+    val email: String,
+    val name: String,
+    val pw: String,
+    val year: Int,
+    val gender: String,
+    val schoolName: String,
+    val majorId: Int
+)
