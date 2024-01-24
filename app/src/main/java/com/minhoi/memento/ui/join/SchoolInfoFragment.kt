6160a7335493c4e121c049bcc275ae08f32117bb
MinhoiKoo <minlove2013@gmail.com>
2024-01-24 16:30:22 +0900

    feat: 회원가입시 유효성 검사 구현
    - 각 입력 observe패턴으로 구현하여 실시간으로 관찰하여 값이 없거나 유효한 값이 아닐경우 가입 진행되지 못하도록 구현

--- app/src/main/java/com/minhoi/memento/ui/join/SchoolInfoFragment.kt
+++ app/src/main/java/com/minhoi/memento/ui/join/SchoolInfoFragment.kt
@@ -1,6 +1,7 @@
 package com.minhoi.memento.ui.join
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,11 +15,12 @@ import com.minhoi.memento.databinding.FragmentSchoolInfoBinding
 import kotlinx.coroutines.launch
 
 
-class SchoolInfoFragment : Fragment() {
+class SchoolInfoFragment : Fragment(), YearPickerDialogFragment.YearPickerDialogListener {
+    private val TAG = SchoolInfoFragment::class.simpleName
 
     private lateinit var binding: FragmentSchoolInfoBinding
     private val joinViewModel: JoinViewModel by activityViewModels()
-    private lateinit var adapter: ArrayAdapter<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -30,24 +32,62 @@ class SchoolInfoFragment : Fragment() {
         // Inflate the layout for this fragment
         val fragmentBinding = FragmentSchoolInfoBinding.inflate(inflater, container, false)
         binding = fragmentBinding
+        binding.viewmodel = joinViewModel
+        binding.lifecycleOwner = this
         return fragmentBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpSchoolNames()
+        observeSchoolInput()
+        observeYearInput()
 
         binding.infoNextBtn.setOnClickListener {
-            // 유효성 검사 진행 해야함
-            lifecycleScope.launch {
-                clickNextBtn()
+            // 입력한 학교 이름이 Schools에 있는지 검사 해야함
+
+            if (isSchoolBlank()) {
+                binding.apply {
+                    inputSchoolName.setBackgroundResource(R.drawable.round_corner_red_color)
+                    schoolHint.text = "학교 이름은 필수 입력사항입니다."
+                }
             }
+
+            if (!isValidSchoolName()) {
+                binding.apply {
+                    inputSchoolName.setBackgroundResource(R.drawable.round_corner_red_color)
+                    schoolHint.text = "존재하지 않는 학교 이름입니다."
+                }
+            }
+
+            if (isYearBlank()) {
+                binding.apply {
+                    inputYear.setBackgroundResource(R.drawable.round_corner_red_color)
+                    yearHint.text = "학번은 필수 선택사항입니다."
+                }
+            }
+
+            if (!isSchoolBlank() && !isYearBlank() && isValidSchoolName()) {
+                lifecycleScope.launch {
+                    clickNextBtn()
+                }
+            }
+        }
+
+        // 학번 선택 클릭시 NumberPickerDialog 호출
+        binding.inputYear.setOnClickListener {
+            showYearPickerDialog()
         }
     }
 
     private fun setUpSchoolNames() {
         joinViewModel.schools.observe(viewLifecycleOwner) { schools ->
-            binding.inputSchoolName.setAdapter(ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, schools.map { it.name }))
+            binding.inputSchoolName.setAdapter(
+                ArrayAdapter(
+                    requireContext(),
+                    android.R.layout.simple_dropdown_item_1line,
+                    schools.map { it.name })
+            )
         }
     }
 
@@ -56,5 +96,43 @@ class SchoolInfoFragment : Fragment() {
         joinViewModel.getMajorsBySchool(schoolName)
         findNavController().navigate(R.id.action_schoolInfoFragment_to_schoolMajorFragment)
     }
+
+    private fun showYearPickerDialog() {
+        val yearPickerDialog = YearPickerDialogFragment()
+        yearPickerDialog.setYearPickerDialogListener(this)
+        yearPickerDialog.show(parentFragmentManager, "YearPickerDialog")
+    }
+
+    // YearPickerDialogListener에서 구현한 콜백 메서드
+    override fun onYearSelected(year: Int) {
+        binding.inputYear.text = year.toString()
+        Log.d("AFragment", "Selected Year: $year")
+    }
+
+    private fun observeSchoolInput() {
+        joinViewModel.school.observe(viewLifecycleOwner) { input ->
+            if (!input.isNullOrBlank()) {
+                binding.inputSchoolName.setBackgroundResource(R.drawable.round_corner_purple_color)
+                binding.schoolHint.text = ""
+            }
+        }
+    }
+
+    private fun observeYearInput() {
+        joinViewModel.year.observe(viewLifecycleOwner) { input ->
+            if (!input.isNullOrBlank()) {
+                binding.inputYear.setBackgroundResource(R.drawable.round_corner_purple_color)
+                binding.yearHint.text = ""
+            }
+        }
+    }
+
+    private fun isValidSchoolName(): Boolean {
+        val schools = joinViewModel.schools.value!!.map { it.name }
+        return schools.contains(joinViewModel.school.value.toString())
+    }
+
+    private fun isSchoolBlank() = joinViewModel.school.value.isNullOrBlank()
+    private fun isYearBlank() = joinViewModel.year.value.isNullOrBlank()
 
 }
