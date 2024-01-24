9e50e4f273d952366ad9de0eda3475fd20b78de2
MinhoiKoo <minlove2013@gmail.com>
2024-01-25 05:34:49 +0900

    feat: 로그인 함수 구현

--- app/src/main/java/com/minhoi/memento/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/minhoi/memento/ui/login/LoginViewModel.kt
@@ -1,9 +1,13 @@
 package com.minhoi.memento.ui.login
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.minhoi.memento.MentoApplication
+import com.minhoi.memento.data.dto.LoginRequest
 import com.minhoi.memento.repository.LoginRepository
-
+import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel() {
     private val loginRepository = LoginRepository()
@@ -17,6 +21,22 @@ class LoginViewModel : ViewModel() {
 
     fun onPasswordTextChanged(text: CharSequence) {
         _password.value = text.toString()
+    }
+
+    suspend fun signIn() {
+        val email = _email.value.toString()
+        val password = _password.value.toString()
+
+        val loginRequest = LoginRequest(email, password)
+        viewModelScope.launch {
+            val response = loginRepository.signIn(loginRequest)
+            if (response.isSuccessful) {
+                response.body()?.let {
+                    MentoApplication.prefs.setAccessToken(it.accessToken)
+                    MentoApplication.prefs.setRefreshToken(it.refreshToken)
+                }
+            }
+        }
     }
 
 }
