f32c95bdd328750ed89794f0adccac89d5fbf513
MinhoiKoo <minlove2013@gmail.com>
2024-01-24 16:14:19 +0900

    feat: BaseActivity 상속 및 viewModel 선언

--- app/src/main/java/com/minhoi/memento/ui/join/JoinActivity.kt
+++ app/src/main/java/com/minhoi/memento/ui/join/JoinActivity.kt
@@ -1,17 +1,23 @@
     package com.minhoi.memento.ui.join
 
-    import androidx.appcompat.app.AppCompatActivity
     import android.os.Bundle
+    import androidx.lifecycle.ViewModelProvider
     import androidx.navigation.NavController
     import androidx.navigation.findNavController
     import com.minhoi.memento.R
+    import com.minhoi.memento.base.BaseActivity
+    import com.minhoi.memento.databinding.ActivityJoinBinding
 
-    class JoinActivity : AppCompatActivity() {
+    class JoinActivity : BaseActivity<ActivityJoinBinding>() {
 
+        override val layoutResourceId: Int = R.layout.activity_join
+
+        private lateinit var viewModel: JoinViewModel
         private lateinit var navController: NavController
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_join)
+            viewModel = ViewModelProvider(this)[JoinViewModel::class.java]
+
         }
 
         override fun onSupportNavigateUp(): Boolean {
