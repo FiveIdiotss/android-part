11d4ca2ecfbb76be8e7cfd6efa8ab1d16afc6eda
MinhoiKoo <minlove2013@gmail.com>
2024-01-27 22:32:37 +0900

    feat: BaseFragment Class 구현
    - Fragment에서 공통으로 필요 부분 선언

---
+++ app/src/main/java/com/minhoi/memento/base/BaseFragment.kt
@@ -0,0 +1,40 @@
+package com.minhoi.memento.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<T: ViewDataBinding> : Fragment() {
+    lateinit var binding: T
+    abstract val layoutResourceId: Int
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutResourceId, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.lifecycleOwner = this@BaseFragment
+        initView()
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    abstract fun initView()
+
+    override fun onDestroyView() {
+        binding.unbind()
+        super.onDestroyView()
+    }
+
+    protected inline fun bind(block: T.() -> Unit) {
+        binding.apply(block)
+    }
+}
