2dc847b347e7968b5c5695faa3d0181d6baf12cd
MinhoiKoo <minlove2013@gmail.com>
2024-02-01 23:50:38 +0900

    feat: 게시글 Dto 변수 추가

--- app/src/main/java/com/minhoi/memento/data/dto/BoardContentDto.kt
+++ app/src/main/java/com/minhoi/memento/data/dto/BoardContentDto.kt
@@ -3,18 +3,27 @@ package com.minhoi.memento.data.dto
 import com.google.gson.annotations.SerializedName
 
 data class BoardContentDto(
+    @SerializedName("boardId")
+    val boardId: Long,
+
+    @SerializedName("memberName")
+    val memberName: String,
+
     @SerializedName("title")
     val title: String,
 
-    @SerializedName("school")
+    @SerializedName("schoolName")
     val school: String,
 
-    @SerializedName("major")
+    @SerializedName("majorName")
     val major: String,
 
     @SerializedName("year")
     val year: Int,
 
     @SerializedName("content")
-    val content: String
+    val content: String,
+
+    @SerializedName("memberId")
+    val memberId: Long
 )
