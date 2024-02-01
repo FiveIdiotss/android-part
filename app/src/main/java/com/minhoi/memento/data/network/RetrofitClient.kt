f81a7dfc41212db78ee223b5c1870ff37c577705
MinhoiKoo <minlove2013@gmail.com>
2024-02-01 23:49:11 +0900

    refactor: Retrofit 객체 공통 부분 분리하여 로그인 상태에서 사용하는 객체와 아닌 객체 분류

--- app/src/main/java/com/minhoi/memento/data/network/RetrofitClient.kt
+++ app/src/main/java/com/minhoi/memento/data/network/RetrofitClient.kt
@@ -1,16 +1,30 @@
 package com.minhoi.memento.data.network
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 
 object RetrofitClient {
     private val baseUrl = "http://43.200.174.186:8080"
 
-    private fun getRetrofit() = Retrofit.Builder()
-        .baseUrl(baseUrl)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+    private val okHttpClient = OkHttpClient.Builder().addInterceptor(AuthInterceptor()).build()
 
-    fun getInstance(): Retrofit = getRetrofit()
+    private fun createRetrofitBuilder(baseUrl: String, client: OkHttpClient? = null): Retrofit.Builder =
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .apply { client?.let { client(it) } }
+
+    private val getLoggedOutRetrofit = createRetrofitBuilder(baseUrl).build()
+
+    private val getLoggedInRetrofit = createRetrofitBuilder(baseUrl, okHttpClient).build()
+
+    fun getInstance(): Retrofit = getLoggedOutRetrofit
+
+    fun getLoggedInInstance(): Retrofit = getLoggedInRetrofit
 
 }
