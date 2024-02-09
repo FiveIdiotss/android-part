071993c8f0b1d9d655957458e6809829af54f7b9
MinhoiKoo <minlove2013@gmail.com>
2024-02-09 18:10:02 +0900

    feat: 게시판 입력한 글 수 만큼씩 불러오는 함수 구현
    - paging3 사용

--- app/src/main/java/com/minhoi/memento/ui/board/BoardViewModel.kt
+++ app/src/main/java/com/minhoi/memento/ui/board/BoardViewModel.kt
@@ -1,12 +1,28 @@
 package com.minhoi.memento.ui.board
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
+import com.minhoi.memento.data.dto.BoardContentDto
 import com.minhoi.memento.repository.BoardRepository
+import kotlinx.coroutines.launch
 
 class BoardViewModel() : ViewModel() {
     private val boardRepository = BoardRepository()
 
-    val boardResponse = boardRepository.getMenteeBoardsStream(5).cachedIn(viewModelScope)
+    private val _post = MutableLiveData<BoardContentDto>()
+    val post: LiveData<BoardContentDto> = _post
+
+    val getBoardsStream = boardRepository.getMenteeBoardsStream(5).cachedIn(viewModelScope)
+
+    fun getBoardContent(boardId: Long) {
+        viewModelScope.launch {
+            val response = boardRepository.getBoardContent(boardId)
+            if (response.isSuccessful) {
+                _post.value = response.body()?.boardDTO
+            }
+        }
+    }
 }
