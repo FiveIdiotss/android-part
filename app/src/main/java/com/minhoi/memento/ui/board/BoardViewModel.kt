23e197ee89be4b2a3402c81d8072b8bf04d8113c
MinhoiKoo <minlove2013@gmail.com>
2024-02-21 23:42:11 +0900

    refactor: BoardContentDto -> BoardContentResponse 로 변경

--- app/src/main/java/com/minhoi/memento/ui/board/BoardViewModel.kt
+++ app/src/main/java/com/minhoi/memento/ui/board/BoardViewModel.kt
@@ -5,15 +5,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
-import com.minhoi.memento.data.dto.BoardContentDto
+import com.minhoi.memento.data.dto.BoardContentResponse
 import com.minhoi.memento.repository.BoardRepository
 import kotlinx.coroutines.launch
 
 class BoardViewModel() : ViewModel() {
     private val boardRepository = BoardRepository()
 
-    private val _post = MutableLiveData<BoardContentDto>()
-    val post: LiveData<BoardContentDto> = _post
+    private val _post = MutableLiveData<BoardContentResponse>()
+    val post: LiveData<BoardContentResponse> = _post
 
     val getBoardsStream = boardRepository.getMenteeBoardsStream(5).cachedIn(viewModelScope)
 
