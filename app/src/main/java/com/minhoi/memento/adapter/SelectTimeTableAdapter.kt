854371792ad1507e622909bc65674cb2f081941c
MinhoiKoo <minlove2013@gmail.com>
2024-02-22 19:46:33 +0900

    feat: 1개만 선택할 수 있도록 작동하는 함수 구현

--- app/src/main/java/com/minhoi/memento/adapter/SelectTimeTableAdapter.kt
+++ app/src/main/java/com/minhoi/memento/adapter/SelectTimeTableAdapter.kt
@@ -3,15 +3,32 @@ package com.minhoi.memento.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.minhoi.memento.R
 import com.minhoi.memento.databinding.TimetableSelectItemBinding
+import com.minhoi.memento.utils.setOnSingleClickListener
 
 class SelectTimeTableAdapter : RecyclerView.Adapter<SelectTimeTableAdapter.TimeTableViewHolder>() {
 
     private val timeTable = mutableListOf<String>()
+    private var selectedPosition = -1
 
     inner class TimeTableViewHolder(private val binding: TimetableSelectItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.root.setOnSingleClickListener {
+                when (selectedPosition) {
+                    -1 -> setSingleSelection(bindingAdapterPosition)
+                    else -> {
+                        notifyItemChanged(selectedPosition)
+                        setSingleSelection(bindingAdapterPosition)
+                    }
+                }
+                notifyItemChanged(selectedPosition)
+            }
+        }
+
         fun bind(item: String) {
-            binding.startTime.text = item
+            binding.startTime.text = item.substring(0,5)
+            onSelectListener(timeTable[bindingAdapterPosition])
         }
     }
 
@@ -24,11 +41,21 @@ class SelectTimeTableAdapter : RecyclerView.Adapter<SelectTimeTableAdapter.TimeT
 
     override fun onBindViewHolder(holder: TimeTableViewHolder, position: Int) {
         holder.bind(timeTable[position])
+        if (selectedPosition == position) {
+            holder.itemView.setBackgroundResource(R.drawable.timetable_selected)
+        } else {
+            holder.itemView.setBackgroundResource(R.drawable.timetable_unselected)
+        }
     }
 
+    // Adapter 생성 후 최초 1회만 실행되는 함수
     fun setList(items: List<String>) {
         timeTable.clear()
         timeTable.addAll(items)
         notifyDataSetChanged()
+    }
+
+    fun setSingleSelection(position: Int) {
+        selectedPosition = position
     }
 }
