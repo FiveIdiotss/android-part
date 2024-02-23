0b9423e1a2c8ce0c384665747d582233eca5834b
MinhoiKoo <minlove2013@gmail.com>
2024-02-24 02:48:50 +0900

    feat: MypageFragment 생성
    - 멘토링 신청, 신청받은 내역 여부 확인 가능

---
+++ app/src/main/java/com/minhoi/memento/ui/mypage/MypageFragment.kt
@@ -0,0 +1,24 @@
+package com.minhoi.memento.ui.mypage
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.minhoi.memento.R
+import com.minhoi.memento.base.BaseFragment
+import com.minhoi.memento.databinding.FragmentMypageBinding
+import com.minhoi.memento.utils.setOnSingleClickListener
+
+class MypageFragment : BaseFragment<FragmentMypageBinding>() {
+
+    override val layoutResourceId: Int = R.layout.fragment_mypage
+
+    override fun initView() {
+        binding.applyListBtn.setOnSingleClickListener {
+            startActivity(Intent(requireContext(), ApplyListActivity::class.java))
+        }
+    }
+
+}
