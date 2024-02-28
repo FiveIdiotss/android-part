78c77a39676c5b50b5f7cca3339f87bf20f5d5a3
MinhoiKoo <minlove2013@gmail.com>
2024-02-28 21:15:10 +0900

    feat: 멘토링 신청 수락/거절 버튼 눌렀을 때 UiState에 따라 사용자에게 각 상황에 맞게 보여주는 기능 구현

--- app/src/main/java/com/minhoi/memento/ui/mypage/received/ReceivedContentActivity.kt
+++ app/src/main/java/com/minhoi/memento/ui/mypage/received/ReceivedContentActivity.kt
@@ -3,17 +3,25 @@ package com.minhoi.memento.ui.mypage.received
 import android.os.Build
 import android.view.View
 import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.minhoi.memento.R
 import com.minhoi.memento.base.BaseActivity
 import com.minhoi.memento.data.dto.MentoringReceivedDto
 import com.minhoi.memento.databinding.ActivityReceivedContentBinding
+import com.minhoi.memento.ui.UiState
 import com.minhoi.memento.ui.mypage.MypageViewModel
 import com.minhoi.memento.utils.ProgressDialog
 import com.minhoi.memento.utils.setOnSingleClickListener
+import com.minhoi.memento.utils.showToast
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class ReceivedContentActivity : BaseActivity<ActivityReceivedContentBinding>() {
     override val layoutResourceId: Int = R.layout.activity_received_content
     private val viewModel: MypageViewModel by viewModels()
+    private var progressDialog: ProgressDialog? = null
 
     override fun initView() {
         val intent = intent
@@ -47,5 +55,53 @@ class ReceivedContentActivity : BaseActivity<ActivityReceivedContentBinding>() {
                 viewModel.rejectApply(receivedContent!!.applyId)
             }
         }
+
+        observeAcceptState()
+        observeRejectState()
     }
+
+    private fun observeAcceptState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.acceptState.collectLatest { state ->
+                    branchState(state)
+                }
+            }
+        }
+    }
+
+    private fun observeRejectState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.rejectState.collectLatest { state ->
+                    branchState(state)
+                }
+            }
+        }
+    }
+
+    private fun branchState(state: UiState) {
+        when (state) {
+            // do nothing
+            is UiState.Empty -> {}
+
+            is UiState.Success<*>-> {
+                showToast("수락 완료")
+                progressDialog?.dismiss()
+                finish()
+            }
+            // show progressbar
+            is UiState.Loading -> {
+                progressDialog = ProgressDialog().apply {
+                    show(supportFragmentManager, tag)
+                }
+            }
+            is UiState.Error -> {
+                progressDialog?.dismiss()
+                showToast("일시적인 오류가 발생하였습니다. 다시 시도해주세요")
+                finish()
+            }
+        }
+    }
+
 }
