2d0b141e2149e2081184c9807095cf8a9767d38a
MinhoiKoo <minlove2013@gmail.com>
2024-03-03 19:06:29 +0900

    feat: ApplyStatus에 따라 신청 상태 표시하는 기능 추가

--- app/src/main/java/com/minhoi/memento/adapter/ApplyListAdapter.kt
+++ app/src/main/java/com/minhoi/memento/adapter/ApplyListAdapter.kt
@@ -1,26 +1,40 @@
 package com.minhoi.memento.adapter
 
 import android.view.LayoutInflater
-import android.view.View.OnClickListener
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.minhoi.memento.data.dto.MentoringApplyDto
 import com.minhoi.memento.databinding.ApplyListRowItemBinding
+import com.minhoi.memento.utils.ApplyStatus
 import com.minhoi.memento.utils.setOnSingleClickListener
 
 class ApplyListAdapter(private val onClickListener: (MentoringApplyDto) -> Unit) : RecyclerView.Adapter<ApplyListAdapter.ViewHolder>() {
 
-    private val applies = mutableListOf<MentoringApplyDto>()
+    private val applies = mutableListOf<Pair<MentoringApplyDto, ApplyStatus>>()
 
     inner class ViewHolder(private val binding: ApplyListRowItemBinding) : RecyclerView.ViewHolder(binding.root) {
         init {
             binding.root.setOnSingleClickListener {
-                onClickListener(applies[bindingAdapterPosition])
+                onClickListener(applies[bindingAdapterPosition].first)
             }
         }
 
-        fun bind(item: MentoringApplyDto) {
-            binding.applyTitle.text = item.boardTitle
+        fun bind(item: Pair<MentoringApplyDto, ApplyStatus>) {
+            binding.applyTitle.text = item.first.boardTitle
+            when (item.second) {
+                ApplyStatus.ACCEPTANCE_PENDING -> {
+                    binding.applyStatus.text = "승인 대기중"
+                    binding.applyStatus.setTextColor(binding.root.context.getColor(android.R.color.holo_blue_light))
+                }
+                ApplyStatus.ACCEPTED -> {
+                    binding.applyStatus.text = "승인 완료"
+                    binding.applyStatus.setTextColor(binding.root.context.getColor(android.R.color.holo_green_light))
+                }
+                ApplyStatus.REJECTED -> {
+                    binding.applyStatus.text = "승인 거절"
+                    binding.applyStatus.setTextColor(binding.root.context.getColor(android.R.color.holo_red_light))
+                }
+            }
         }
     }
 
@@ -35,7 +49,7 @@ class ApplyListAdapter(private val onClickListener: (MentoringApplyDto) -> Unit)
         holder.bind(applies[position])
     }
 
-    fun setList(contents: List<MentoringApplyDto>) {
+    fun setList(contents: List<Pair<MentoringApplyDto, ApplyStatus>>) {
         applies.clear()
         applies.addAll(contents)
         notifyDataSetChanged()
