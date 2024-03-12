52f46091b19cd5922ea9050d59db939bd3a8f1e9
MinhoiKoo <minlove2013@gmail.com>
2024-03-12 21:37:08 +0900

    feat: 채팅 상대 정보에 따라 방 ID 가져오는 함수 구현

--- app/src/main/java/com/minhoi/memento/data/network/service/ChatService.kt
+++ app/src/main/java/com/minhoi/memento/data/network/service/ChatService.kt
@@ -3,9 +3,12 @@ package com.minhoi.memento.data.network.service
 import com.minhoi.memento.data.dto.chat.ChatRoom
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ChatService {
     @GET("api/chat/chatRooms")
-    suspend fun getChatRooms(@Path("memberId") memberId: Long): Response<List<ChatRoom>>
+    suspend fun getChatRooms(@Query("memberId") memberId: Long): Response<List<ChatRoom>>
+
+    @GET("api/chat/chatRoom")
+    suspend fun getRoomId(@Query("receiverId") receiverId: Long): Response<ChatRoom>
 }
