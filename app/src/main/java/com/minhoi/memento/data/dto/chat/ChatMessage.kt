e646d1a33a912ac806016cc8d8fb5c509e7a6467
MinhoiKoo <minlove2013@gmail.com>
2024-03-18 23:12:55 +0900

    refactor: Chat 관련 Data class 수정
    - id 추가

--- app/src/main/java/com/minhoi/memento/data/dto/chat/ChatMessage.kt
+++ app/src/main/java/com/minhoi/memento/data/dto/chat/ChatMessage.kt
@@ -1,6 +1,7 @@
 package com.minhoi.memento.data.dto.chat
 
 interface ChatMessage {
+    val id: Long
     val content: String
     val date: String
     val name: String
--- app/src/main/java/com/minhoi/memento/data/dto/chat/Receiver.kt
+++ app/src/main/java/com/minhoi/memento/data/dto/chat/Receiver.kt
@@ -1,7 +1,7 @@
 package com.minhoi.memento.data.dto.chat
 
 data class Receiver(
-    val id: Long,
+    override val id: Long,
     override val name: String,
     override val content: String,
     override val date: String
