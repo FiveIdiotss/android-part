2825149dd08f37117720b0d30768d77c051e7de7
MinhoiKoo <minlove2013@gmail.com>
2024-03-19 22:39:40 +0900

    feat: 채팅방 마지막 보낸 날짜가 오늘이면 보낸 시간, 오늘이 아니면 보낸 날짜 표시하는 BindingAdapter 구현

--- app/src/main/java/com/minhoi/memento/utils/BindingAdapter.kt
+++ app/src/main/java/com/minhoi/memento/utils/BindingAdapter.kt
@@ -1,9 +1,12 @@
 package com.minhoi.memento.utils
 
 import android.widget.CalendarView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.minhoi.memento.ui.board.BoardViewModel
-import java.util.Calendar
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 object BindingAdapter {
     @BindingAdapter("onDateChanged")
@@ -12,6 +15,23 @@ object BindingAdapter {
         viewModel ?: return
         calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
             viewModel.onDateSelected(year, month, dayOfMonth)
+        }
+    }
+
+    /**
+     * 마지막 메세지 보낸 날짜가 오늘이면 시간만 표시하고 아니면 날짜만 표시하는 함수
+     */
+    @BindingAdapter("formatChatRoomDate")
+    @JvmStatic
+    fun formatChatRoomDate(view: TextView, date: String?) {
+        date?.let {
+            val today = LocalDate.now()
+
+            if (LocalDateTime.parse(date).toLocalDate().isEqual(today)) {
+                view.text = parseLocalDateTime(date.toString()).substring(0, 8)
+            } else {
+                view.text = date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+            }
         }
     }
 }
