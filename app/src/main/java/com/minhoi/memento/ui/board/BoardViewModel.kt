76f3fd37fe3d6327c4e3fe6b479b019b8b6fec28
MinhoiKoo <minlove2013@gmail.com>
2024-03-28 23:35:48 +0900

    feat: 멤버의 북마크 목록을 성공적으로 가져왔을 경우 멘토링 게시글 Flow 구독하여 북마크 목록과 비교하는 로직 추가

--- app/src/main/java/com/minhoi/memento/ui/board/BoardViewModel.kt
+++ app/src/main/java/com/minhoi/memento/ui/board/BoardViewModel.kt
@@ -5,22 +5,34 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import androidx.paging.map
+import com.minhoi.memento.data.dto.BoardContentDto
 import com.minhoi.memento.data.dto.BoardContentResponse
 import com.minhoi.memento.data.dto.MentoringApplyRequest
 import com.minhoi.memento.repository.BoardRepository
 import com.minhoi.memento.data.model.DayOfWeek
+import com.minhoi.memento.data.network.ApiResult
+import com.minhoi.memento.repository.MemberRepository
+import com.minhoi.memento.ui.UiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.time.LocalDate
 
 class BoardViewModel() : ViewModel() {
     private val boardRepository = BoardRepository()
+    private val memberRepository = MemberRepository()
 
     private val _post = MutableLiveData<BoardContentResponse>()
     val post: LiveData<BoardContentResponse> = _post
 
-    val getBoardsStream = boardRepository.getMenteeBoardsStream(5).cachedIn(viewModelScope)
-
+    private val _boardList = MutableStateFlow<UiState<PagingData<BoardContentDto>>>(UiState.Empty)
+    val boardList: StateFlow<UiState<PagingData<BoardContentDto>>> = _boardList.asStateFlow()
     private val _isAvailableDay = MutableLiveData<Boolean>()
     val isAvailableDay: LiveData<Boolean> = _isAvailableDay
 
@@ -29,6 +41,32 @@ class BoardViewModel() : ViewModel() {
 
     init {
         _isAvailableDay.value = false
+        getBoardStream()
+    }
+
+    fun getBoardStream() {
+        viewModelScope.launch {
+            memberRepository.getBookmarkBoards().collectLatest { bookmarkBoards ->
+                if (bookmarkBoards is ApiResult.Success) {
+                    boardRepository.getMenteeBoardsStream(5).cachedIn(viewModelScope)
+                        .collectLatest { boards ->
+                            val transformedPagingData = boards.map { boardContentDto ->
+                                // 즐겨찾기 목록에 있는지 확인하고, 상태를 업데이트합니다.
+                                if (bookmarkBoards.value.any { bookmarkBoard ->
+                                        bookmarkBoard.boardId == boardContentDto.boardId
+                                    }) {
+                                    boardContentDto.apply { isBookmarked = true }
+                                } else {
+                                    boardContentDto
+                                }
+                            }
+                            _boardList.update { UiState.Success(transformedPagingData) }
+                        }
+                } else {
+                    _boardList.update { UiState.Error(Throwable("BookmarkBoards is null")) }
+                }
+            }
+        }
     }
 
     fun getBoardContent(boardId: Long) {
