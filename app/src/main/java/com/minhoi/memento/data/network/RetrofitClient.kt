340acc4ee68b80aa3d4df1d122023193cecc469e
MinhoiKoo <minlove2013@gmail.com>
2024-04-04 01:33:15 +0900

    refactor: 서버 IP BuildConfig에서 불러오도록 변경

--- app/src/main/java/com/minhoi/memento/data/network/RetrofitClient.kt
+++ app/src/main/java/com/minhoi/memento/data/network/RetrofitClient.kt
@@ -2,14 +2,16 @@ package com.minhoi.memento.data.network
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.minhoi.memento.BuildConfig
+import com.minhoi.memento.data.network.interceptor.AuthInterceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
 object RetrofitClient {
-    private val baseUrl = "http://43.200.174.186:8080"
-
+    private const val baseUrl = "http://" + BuildConfig.SERVER_IP + ":8080/"
+    private val gson: Gson = GsonBuilder().setLenient().create()
     private val okHttpClient = OkHttpClient.Builder().addInterceptor(AuthInterceptor()).build()
 
     private fun createRetrofitBuilder(baseUrl: String, client: OkHttpClient? = null): Retrofit.Builder =
