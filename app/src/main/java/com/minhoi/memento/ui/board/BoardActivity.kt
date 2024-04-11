8e10d003331d3aa145aa76d77d46c5bc6e374b7c
MinhoiKoo <minlove2013@gmail.com>
2024-04-11 16:01:19 +0900

    feat: 북마크 버튼 누를 경우 viewModel의 bookmarkState 관찰하여 UI 업데이트 하는 로직 추가

--- app/src/main/java/com/minhoi/memento/ui/board/BoardActivity.kt
+++ app/src/main/java/com/minhoi/memento/ui/board/BoardActivity.kt
@@ -1,19 +1,31 @@
 package com.minhoi.memento.ui.board
 
 import android.content.Intent
+import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.minhoi.memento.R
 import com.minhoi.memento.base.BaseActivity
 import com.minhoi.memento.databinding.ActivityBoardBinding
+import com.minhoi.memento.ui.UiState
+import com.minhoi.memento.utils.hideLoading
 import com.minhoi.memento.utils.setOnSingleClickListener
+import com.minhoi.memento.utils.showLoading
+import com.minhoi.memento.utils.showToast
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class BoardActivity : BaseActivity<ActivityBoardBinding>() {
     private val viewModel by viewModels<BoardViewModel>()
+    private var boardId: Long = -1
     override val layoutResourceId: Int = R.layout.activity_board
 
     override fun initView() {
-        val boardId = intent.getLongExtra("boardId", -1L)
+        boardId = intent.getLongExtra("boardId", -1L)
         getBoardContent(boardId)
         setSupportActionBar(binding.toolbar)
         supportActionBar?.apply {
@@ -28,6 +40,12 @@ class BoardActivity : BaseActivity<ActivityBoardBinding>() {
             startActivity(Intent(this, ApplyMentoringActivity::class.java))
         }
         observeBoardContent()
+        observeBookmarkState()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.board_toolbar_menu, menu)
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -35,6 +53,9 @@ class BoardActivity : BaseActivity<ActivityBoardBinding>() {
             android.R.id.home -> {
                 finish()
                 return true
+            }
+            R.id.action_bookmark -> {
+                viewModel.boardBookmarkState.value?.let { viewModel.executeBookmark(boardId, it) }
             }
         }
         return super.onOptionsItemSelected(item)
@@ -69,6 +90,17 @@ class BoardActivity : BaseActivity<ActivityBoardBinding>() {
                         }
                     }
                 }
+            }
+        }
+    }
+    private fun observeBookmarkState() {
+        viewModel.boardBookmarkState.observe(this) { state ->
+            if (state) {
+                binding.toolbar.menu.findItem(R.id.action_bookmark).icon =
+                    ContextCompat.getDrawable(this, R.drawable.heart_filled)
+            } else {
+                binding.toolbar.menu.findItem(R.id.action_bookmark).icon =
+                    ContextCompat.getDrawable(this, R.drawable.heart_empty)
             }
         }
     }
