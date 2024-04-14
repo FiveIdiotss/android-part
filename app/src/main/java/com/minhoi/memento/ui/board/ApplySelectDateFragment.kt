7789c6c00cbb1a1dfb07fa35287bf929d2a1842c
MinhoiKoo <minlove2013@gmail.com>
2024-04-14 23:04:25 +0900

    fix: 상담 날짜, 시간 미선택시 다음 페이지로 넘어가지 못하도록 설정

--- app/src/main/java/com/minhoi/memento/ui/board/ApplySelectDateFragment.kt
+++ app/src/main/java/com/minhoi/memento/ui/board/ApplySelectDateFragment.kt
@@ -2,12 +2,15 @@ package com.minhoi.memento.ui.board
 
 import android.util.Log
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.minhoi.memento.R
 import com.minhoi.memento.adapter.SelectTimeTableAdapter
 import com.minhoi.memento.base.BaseFragment
 import com.minhoi.memento.data.dto.TimeTableDto
 import com.minhoi.memento.databinding.FragmentApplySelectDateBinding
+import com.minhoi.memento.utils.setOnSingleClickListener
+import com.minhoi.memento.utils.showToast
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
@@ -28,6 +31,17 @@ class ApplySelectDateFragment : BaseFragment<FragmentApplySelectDateBinding>() {
             adapter = timeTableAdapter
             layoutManager = GridLayoutManager(requireContext(), 3)
         }
+
+        binding.nextBtn.setOnSingleClickListener {
+            with(viewModel) {
+                when {
+                    getSelectedDate().isEmpty() -> requireContext().showToast("상담 날짜는 필수 선택사항입니다.")
+                    getSelectedTime().isEmpty() -> requireContext().showToast("상담 시간은 필수 선택사항입니다.")
+                    else -> findNavController().navigate(R.id.action_applySelectDateFragment_to_applyShowResultFragment)
+                }
+            }
+        }
+
         observeTimeTable()
     }
 
