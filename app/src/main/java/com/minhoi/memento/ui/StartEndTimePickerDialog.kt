9a9fa6d946270704f67c2bdcff508f17185f6232
MinhoiKoo <minlove2013@gmail.com>
2024-04-21 17:38:01 +0900

    refactor: 코드 포맷팅

--- app/src/main/java/com/minhoi/memento/ui/StartEndTimePickerDialog.kt
+++ app/src/main/java/com/minhoi/memento/ui/StartEndTimePickerDialog.kt
@@ -11,20 +11,30 @@ import androidx.fragment.app.DialogFragment
 import com.minhoi.memento.R
 import com.minhoi.memento.data.dto.TimeTableDto
 import com.minhoi.memento.databinding.StartEndTimepickerDialogBinding
-import com.minhoi.memento.utils.overlaps
+import com.minhoi.memento.utils.containsOverlap
 import com.minhoi.memento.utils.setOnSingleClickListener
 import com.minhoi.memento.utils.setTimeInterval
 
 class StartEndTimePickerDialog(
     private val interval: Int,
-    private val onTimeSelected: (TimeTableDto) -> Unit
+    private val timetables: List<TimeTableDto>,
+    private val onTimeSelected: (TimeTableDto) -> Unit,
 ) : DialogFragment() {
 
     private lateinit var binding: StartEndTimepickerDialogBinding
     private val times = mutableListOf<IntRange>()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        binding = DataBindingUtil.inflate(inflater, R.layout.start_end_timepicker_dialog, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.start_end_timepicker_dialog,
+            container,
+            false
+        )
         return binding.root
     }
 
@@ -57,7 +67,6 @@ class StartEndTimePickerDialog(
                 stringTimeToInt(startTime),
                 stringTimeToInt(endTime)
             )
-            Log.d("RANGE", "onViewCreated: $timeRange")
 
             when {
                 binding.startTimePicker.hour > binding.endTimePicker.hour -> {
@@ -66,27 +75,25 @@ class StartEndTimePickerDialog(
 
                 binding.startTimePicker.hour < binding.endTimePicker.hour -> {
                     if (!checkIsDuplicateTime(timeRange)) {
-                        addTime(timeRange, startTime, endTime)
-                        Log.d("TIMES", "onViewCreated: ${"times : $times"}")
+                        addTime(startTime, endTime)
                     } else {
                         Toast.makeText(requireContext(), duplicateTimeMessage, Toast.LENGTH_SHORT)
                             .show()
                     }
                 }
-
+                // 시작 시간과 종료 시간의 hour가 같은 경우
                 else -> {
+                    // 시작 시간의 분이 종료 시간의 분 보다 큰 경우
                     if (binding.startTimePicker.minute >= binding.endTimePicker.minute) {
                         Toast.makeText(requireContext(), invalidTimeMessage, Toast.LENGTH_SHORT)
                             .show()
-                    } else {
-                        if (!checkIsDuplicateTime(timeRange)) {
-                            addTime(timeRange, startTime, endTime)
-                        } else {
-                            Toast.makeText(
-                                requireContext(),
-                                duplicateTimeMessage,
-                                Toast.LENGTH_SHORT
-                            ).show()
+                        return@setOnSingleClickListener
+                    }
+                    // 시작 시간의 분이 종료 시간의 분 보다 작은 경우
+                    if (binding.startTimePicker.minute < binding.endTimePicker.minute) {
+                        when (checkIsDuplicateTime(timeRange)) {
+                            true -> Toast.makeText(requireContext(), duplicateTimeMessage, Toast.LENGTH_SHORT).show()
+                            false -> addTime(startTime, endTime)
                         }
                     }
                 }
@@ -106,18 +113,11 @@ class StartEndTimePickerDialog(
     }
 
     private fun checkIsDuplicateTime(newTime: IntRange): Boolean {
-        var isOverlapped = false
-        for (time in times) {
-            Log.d("RANGE", "check: ${time + newTime}")
-            if (time.overlaps(newTime)) {
-                isOverlapped = true
-                break
-            }
-        }
-        return isOverlapped
+        return times.containsOverlap(newTime)
     }
 
     companion object {
+        private const val TAG = "StartEndTimePickerDialog"
         private const val invalidTimeMessage = "시작 시간이 종료 시간보다 늦을 수 없습니다."
         private const val duplicateTimeMessage = "중복된 시간입니다."
     }
