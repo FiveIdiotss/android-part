3ed20b7fc15fa091e2a68f1f7e67fa4a3ccc41c7
MinhoiKoo <minlove2013@gmail.com>
2024-04-29 20:05:52 +0900

    feat: FCM 채널 등록하는 함수 구현

--- app/src/main/java/com/minhoi/memento/MentoApplication.kt
+++ app/src/main/java/com/minhoi/memento/MentoApplication.kt
@@ -1,6 +1,10 @@
 package com.minhoi.memento
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import com.google.firebase.FirebaseApp
 import com.minhoi.memento.utils.MemberPrefs
 import com.minhoi.memento.utils.TokenPrefs
 import dagger.hilt.android.HiltAndroidApp
@@ -17,6 +21,35 @@ class MentoApplication : Application() {
 
         prefs = TokenPrefs(applicationContext)
         memberPrefs = MemberPrefs(applicationContext)
+
+        FirebaseApp.initializeApp(this)
+        addFCMChannel()
     }
 
+    // FCM Channel을 등록하는 함수
+    private fun addFCMChannel() {
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val chatChannel = NotificationChannel(
+            "CHAT_CHANNEL",
+            getString(R.string.channel_chat),
+            NotificationManager.IMPORTANCE_HIGH
+        ).apply {
+            description = getString(R.string.channel_chat_description)
+        }
+        val postChannel = NotificationChannel(
+            "POST_CHANNEL",
+            getString(R.string.channel_post),
+            NotificationManager.IMPORTANCE_HIGH
+        ).apply {
+            description = getString(R.string.channel_post_description)
+        }
+
+        val channels = listOf(
+            chatChannel,
+            postChannel
+        )
+        notificationManager.createNotificationChannels(channels)
+    }
 }
