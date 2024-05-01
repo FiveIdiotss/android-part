f2970039951e1cd9004146b5d1caf2470758b75b
MinhoiKoo <minlove2013@gmail.com>
2024-05-01 23:53:44 +0900

    feat: 안드로이드 버전에 따라 알림 권한 요청 및 권한 승인/거절에 따라 SharedPreference에 각 기능별 알림 권한 저장 기능 구현

--- app/src/main/java/com/minhoi/memento/MainActivity.kt
+++ app/src/main/java/com/minhoi/memento/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.minhoi.memento
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.pm.PackageManager
+import android.os.Build
 import android.util.Log
 import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -14,7 +21,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 import com.google.firebase.messaging.FirebaseMessaging
 import com.minhoi.memento.base.BaseActivity
 import com.minhoi.memento.databinding.ActivityMainBinding
-import com.minhoi.memento.ui.board.MentorPostActivity
 import com.minhoi.memento.ui.home.HomeViewModel
 import com.minhoi.memento.ui.home.PostSelectBottomSheetDialog
 
@@ -24,7 +30,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override fun initView() {
         setBottomNavigation()
-
+        askNotificationPermission()
         FirebaseMessaging.getInstance().token
             .addOnCompleteListener { task: Task<String> ->
                 if (!task.isSuccessful) {
@@ -67,6 +73,47 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
                 false
             }
             else NavigationUI.onNavDestinationSelected(it, navController)
+        }
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            MentoApplication.notificationPermissionPrefs.setChatPermission(true)
+            Toast.makeText(this, "알림 권한 승인", Toast.LENGTH_LONG).show()
+        } else {
+            MentoApplication.notificationPermissionPrefs.setChatPermission(false)
+            Toast.makeText(this, "알림 권한 실패", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    // 사용자가 권한을 거부하면 다시 요청 메세지 띄우기 위해 IntroActivity가 아닌 MainActivity에서 권한 요청
+    private fun askNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            // 알림 권한이 있는 경우
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                MentoApplication.notificationPermissionPrefs.setChatPermission(true)
+                Toast.makeText(this, "이미 승인된 알림 권한", Toast.LENGTH_LONG).show()
+
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                // 이전에 사용자가 알림 권한 요청을 거부했을 경우
+                AlertDialog.Builder(this)
+                    .setTitle("알림 권한 필요")
+                    .setMessage("이 앱은 중요한 소식과 업데이트를 알리기 위해 알림 기능이 필요합니다. 알림 권한을 허용해주세요.")
+                    .setPositiveButton("권한 설정하기") { dialog, which ->
+                        // 권한 요청 다시 진행
+                        requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                    }
+                    .setNegativeButton("나중에") { dialog, which ->
+                        // 사용자가 나중에 결정하길 원할 경우 처리
+                    }
+                    .show()
+            } else {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
         }
     }
 }
