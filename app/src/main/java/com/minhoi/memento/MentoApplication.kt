90597d4521e4c733daf2f09f9cb5120919aea80c
MinhoiKoo <minlove2013@gmail.com>
2024-05-01 23:52:48 +0900

    feat: 알림 권한 저장하는 SharedPreference 선언

--- app/src/main/java/com/minhoi/memento/MentoApplication.kt
+++ app/src/main/java/com/minhoi/memento/MentoApplication.kt
@@ -14,6 +14,7 @@ class MentoApplication : Application() {
     companion object {
         lateinit var prefs: TokenPrefs
         lateinit var memberPrefs: MemberPrefs
+        lateinit var notificationPermissionPrefs: NotificationPermissionPrefs
     }
 
     override fun onCreate() {
@@ -21,6 +22,7 @@ class MentoApplication : Application() {
 
         prefs = TokenPrefs(applicationContext)
         memberPrefs = MemberPrefs(applicationContext)
+        notificationPermissionPrefs = NotificationPermissionPrefs(applicationContext)
 
         FirebaseApp.initializeApp(this)
         addFCMChannel()
