b06a6833c8e99b429c124c8ece026189c7f609f5
MinhoiKoo <minlove2013@gmail.com>
2024-05-21 23:23:14 +0900

    feat: LoadStateListener 구현
    - 로딩중, 성공, 에러, 빈 값인 경우 분기하여 UI 표시

--- app/src/main/java/com/minhoi/memento/ui/board/BoardListActivity.kt
+++ app/src/main/java/com/minhoi/memento/ui/board/BoardListActivity.kt
@@ -3,10 +3,13 @@ package com.minhoi.memento.ui.board
 import android.content.Intent
 import com.minhoi.memento.adapter.BoardAdapter
 import android.view.MenuItem
+import android.view.View
 import androidx.activity.viewModels
+import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.minhoi.memento.R
 import com.minhoi.memento.adapter.BoardLoadStateAdapter
@@ -34,15 +37,39 @@ class BoardListActivity : BaseActivity<ActivityBoardListBinding>() {
     }
 
     override fun initView() {
-
+        binding.viewModel = viewModel
         setUpToolbar()
         category = intent.getStringExtra("category")
         category?.let { binding.filterCategoryBtn.text = category }
         viewModel.setCategoryFilter(category)
+        binding.emptyText = "게시글이 존재하지 않아요"
 
-        binding.apply {
-            boardRv.adapter = boardAdapter.withLoadStateFooter(BoardLoadStateAdapter())
-            boardRv.layoutManager =
+        boardAdapter.addLoadStateListener { combinedLoadStates ->
+            binding.apply {
+                // 로딩 중 일 때
+                progressBar.isVisible = combinedLoadStates.source.refresh is LoadState.Loading
+
+                // 로딩 중이지 않을 때 (활성 로드 작업이 없고 에러가 없음)
+                boardRv.isVisible = combinedLoadStates.source.refresh is LoadState.NotLoading
+
+                // 로딩 에러 발생 시
+                retryLayout.isVisible = combinedLoadStates.source.refresh is LoadState.Error
+
+                if (combinedLoadStates.append.endOfPaginationReached) {
+                    // 아이템 수가 1보다 작으면 Empty View 보이도록 처리
+                    if (boardAdapter.itemCount < 1) {
+                        binding.emptyLayout.root.visibility = View.VISIBLE
+                    } else {
+                        // 아이템 수가 1보다 크면 Empty View 안보이도록 처리
+                        binding.emptyLayout.root.visibility = View.GONE
+                    }
+                }
+            }
+        }
+
+        binding.boardRv.apply {
+            adapter = boardAdapter.withLoadStateFooter(BoardLoadStateAdapter())
+            layoutManager =
                 LinearLayoutManager(this@BoardListActivity, LinearLayoutManager.VERTICAL, false)
         }
 
@@ -87,4 +114,5 @@ class BoardListActivity : BaseActivity<ActivityBoardListBinding>() {
         }
         return super.onOptionsItemSelected(item)
     }
+
 }
