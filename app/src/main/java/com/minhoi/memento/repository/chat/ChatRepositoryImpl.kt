36d7e421c65d4ac6e97a739f5890a291cc1a96fe
MinhoiKoo <minlove2013@gmail.com>
2024-05-23 15:07:49 +0900

    feat: 채팅방에서 파일 전송하는 함수 구현

--- app/src/main/java/com/minhoi/memento/repository/chat/ChatRepositoryImpl.kt
+++ app/src/main/java/com/minhoi/memento/repository/chat/ChatRepositoryImpl.kt
@@ -2,10 +2,10 @@ package com.minhoi.memento.repository.chat
 
 import com.minhoi.memento.data.dto.chat.ChatRoom
 import com.minhoi.memento.data.network.ApiResult
-import com.minhoi.memento.data.network.RetrofitClient
 import com.minhoi.memento.utils.safeFlow
 import com.minhoi.memento.data.network.service.ChatService
 import kotlinx.coroutines.flow.Flow
+import okhttp3.MultipartBody
 import javax.inject.Inject
 
 class ChatRepositoryImpl @Inject constructor(
@@ -22,5 +22,9 @@ class ChatRepositoryImpl @Inject constructor(
 
     override fun getMessages(roomId: Long, page: Int, size: Int) = safeFlow {
         chatService.getChatMessages(roomId, page, size)
+    }
+
+    override fun sendFile(file: MultipartBody.Part, roomId: Long) = safeFlow {
+        chatService.sendFile(file, roomId)
     }
 }
