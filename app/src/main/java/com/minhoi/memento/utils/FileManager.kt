f359efdbb8b9416abca1c9c57da70fd13e726d8b
MinhoiKoo <minlove2013@gmail.com>
2024-05-23 17:57:05 +0900

    refactor: FileManager Object -> Class로 변경 및 context ApplicationContext 주입받도록 DI Module 구현

--- app/src/main/java/com/minhoi/memento/utils/FileManager.kt
+++ app/src/main/java/com/minhoi/memento/utils/FileManager.kt
@@ -1,22 +1,34 @@
 package com.minhoi.memento.utils
 
+import android.content.ContentValues
 import android.content.Context
+import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
 import android.provider.OpenableColumns
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.Target
+import com.minhoi.memento.data.network.SaveFileResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.asRequestBody
 import java.io.File
 import java.io.FileOutputStream
+import javax.inject.Inject
 
-object FileManager {
+class FileManager @Inject constructor(
+    private val context: Context,
+) {
 
-    fun getFileMimeType(context: Context, uri: Uri): String? {
+    fun getFileMimeType(uri: Uri): String? {
         val contentResolver = context.contentResolver
         return contentResolver.getType(uri)
     }
 
-    fun uriToMultipartBodyPart(context: Context, uri: Uri, mimeType: String): MultipartBody.Part? {
+    fun uriToMultipartBodyPart(uri: Uri, mimeType: String): MultipartBody.Part? {
         val contentResolver = context.contentResolver
         val inputStream = contentResolver.openInputStream(uri) ?: return null
         val file = File(context.cacheDir, getFileName(context, uri))
