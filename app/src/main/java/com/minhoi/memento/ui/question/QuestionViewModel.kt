c1fc1e65669246224d6af84af34642c6030cffb1
MinhoiKoo <minlove2013@gmail.com>
2024-06-09 17:38:50 +0900

    feat: 질문 게시판 필터링 기능 구현
    - 카테고리, 우리 학교글, 검색어 Flow 중 하나라도 변경될 경우 새로운 Flow 발행하는 방식으로 구현

--- app/src/main/java/com/minhoi/memento/ui/question/QuestionViewModel.kt
+++ app/src/main/java/com/minhoi/memento/ui/question/QuestionViewModel.kt
@@ -10,16 +10,21 @@ import androidx.paging.cachedIn
 import com.minhoi.memento.data.dto.question.QuestionContent
 import com.minhoi.memento.data.dto.question.QuestionPostRequest
 import com.minhoi.memento.data.dto.question.ReplyContent
-import com.minhoi.memento.pagingsource.QuestionPagingSource
-import com.minhoi.memento.pagingsource.ReplyPagingSource
+import com.minhoi.memento.repository.pagingsource.QuestionPagingSource
+import com.minhoi.memento.repository.pagingsource.ReplyPagingSource
 import com.minhoi.memento.repository.question.QuestionRepository
 import com.minhoi.memento.ui.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -38,13 +43,35 @@ class QuestionViewModel @Inject constructor(
     private val _postQuestionState = MutableStateFlow<UiState<Boolean>>(UiState.Empty)
     val postQuestionState: StateFlow<UiState<Boolean>> = _postQuestionState.asStateFlow()
 
-    fun getQuestions(pageSize: Int): Flow<PagingData<QuestionContent>> {
-        return Pager(
-            config = PagingConfig(pageSize = pageSize),
-            pagingSourceFactory = { QuestionPagingSource(questionRepository) })
-            .flow
-            .cachedIn(viewModelScope)
+    private val categoryQueryFlow = MutableStateFlow<String?>(null)
+    private val schoolFilterFlow = MutableStateFlow<Boolean>(false)
+    private val searchQueryFlow = MutableStateFlow<String?>(null)
+
+    @OptIn(FlowPreview::class)
+    private val questionFilterFlow = combine(
+        schoolFilterFlow,
+        categoryQueryFlow,
+        searchQueryFlow.debounce(300L)
+    ) { schoolFilter, category, searchKeyWord ->
+        Triple(schoolFilter, category, searchKeyWord)
     }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    fun getQuestions(pageSize: Int) =
+        questionFilterFlow.flatMapLatest { (schoolFilter, category, searchKeyWord) ->
+            Pager(
+                config = PagingConfig(pageSize = pageSize),
+                pagingSourceFactory = {
+                    QuestionPagingSource(
+                        questionRepository,
+                        schoolFilter,
+                        category,
+                        searchKeyWord
+                    )
+                })
+                .flow
+                .cachedIn(viewModelScope)
+        }
 
     fun getQuestion(questionId: Long) {
         viewModelScope.launch {
@@ -52,7 +79,7 @@ class QuestionViewModel @Inject constructor(
                 result.handleResponse(
                     onSuccess = { data ->
                         Log.d("QuestionVIewModel", "getQuestion: $data")
-                        _questionContent.update { UiState.Success(data.questionContent) }
+                        _questionContent.update { UiState.Success(data.data.questionContent) }
                     },
                     onError = { error ->
                         _questionContent.update { UiState.Error(error.exception) }
@@ -103,4 +130,15 @@ class QuestionViewModel @Inject constructor(
         }
     }
 
+    fun setCategoryFilter(category: String?) {
+        categoryQueryFlow.update { category }
+    }
+
+    fun setSchoolFilter(isChecked: Boolean) {
+        schoolFilterFlow.update { isChecked }
+    }
+
+    fun setSearchQuery(query: String?) {
+        searchQueryFlow.update { query }
+    }
 }
