b12bb43f429e26e704cc246ee770b6a3a484a35a
MinhoiKoo <minlove2013@gmail.com>
2024-06-10 17:16:21 +0900

    feat: 해당 질문 클릭하면 질문 글로 이동하는 클릭 리스너 설정

--- app/src/main/java/com/minhoi/memento/ui/home/HomeFragment.kt
+++ app/src/main/java/com/minhoi/memento/ui/home/HomeFragment.kt
@@ -13,6 +13,7 @@ import com.minhoi.memento.ui.adapter.BoardAdapter
 import com.minhoi.memento.ui.adapter.QuestionPreviewAdapter
 import com.minhoi.memento.ui.board.BoardActivity
 import com.minhoi.memento.ui.board.BoardListActivity
+import com.minhoi.memento.ui.question.QuestionInfoActivity
 import com.minhoi.memento.ui.question.QuestionListActivity
 import com.minhoi.memento.utils.ColumnItemDecoration
 import com.minhoi.memento.utils.setOnSingleClickListener
@@ -37,12 +38,12 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
                     "boardId", it.boardId
                 ))
             },
-            onBookmarkClickListener = {
-
-            }
+            onBookmarkClickListener = {}
         )
 
-        questionPreviewAdapter = QuestionPreviewAdapter()
+        questionPreviewAdapter = QuestionPreviewAdapter {
+            startActivity(Intent(requireContext(), QuestionInfoActivity::class.java).putExtra("questionId", it))
+        }
 
         binding.toMentorBoard.setOnSingleClickListener {
             startActivity(Intent(requireContext(), BoardListActivity::class.java))
