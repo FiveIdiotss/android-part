5cefc3f4ad0684c6b0c001c8799c74e49b319adc
MinhoiKoo <minlove2013@gmail.com>
2024-06-14 16:54:19 +0900

    refactor: 이미지 추가함에 따라 스크롤이 필요하여 Rv + NestedScrollView 대신 concatAdapter 사용하여 뷰홀더 재사용할 수 있도록 변경

--- app/src/main/java/com/minhoi/memento/ui/question/QuestionInfoActivity.kt
+++ app/src/main/java/com/minhoi/memento/ui/question/QuestionInfoActivity.kt
@@ -8,12 +8,16 @@ import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.minhoi.memento.R
 import com.minhoi.memento.base.BaseActivity
 import com.minhoi.memento.databinding.ActivityQuestionInfoBinding
 import com.minhoi.memento.ui.UiState
 import com.minhoi.memento.ui.adapter.ReplyAdapter
+import com.minhoi.memento.ui.adapter.question.QuestionInfoContentAdapter
+import com.minhoi.memento.ui.adapter.question.QuestionInfoImageAdapter
+import com.minhoi.memento.ui.adapter.question.QuestionInfoLikeReplyCountAdapter
 import com.minhoi.memento.utils.hideKeyboard
 import com.minhoi.memento.utils.hideLoading
 import com.minhoi.memento.utils.setOnSingleClickListener
@@ -28,38 +32,44 @@ class QuestionInfoActivity : BaseActivity<ActivityQuestionInfoBinding>() {
     override val layoutResourceId: Int = R.layout.activity_question_info
     private val viewModel by viewModels<QuestionViewModel>()
     private var questionId: Long = -1
+    private val questionInfoContentAdapter: QuestionInfoContentAdapter by lazy { QuestionInfoContentAdapter() }
+    private val questionInfoImageAdapter: QuestionInfoImageAdapter by lazy { QuestionInfoImageAdapter() }
+    private val questionLikeReplyCountAdapter: QuestionInfoLikeReplyCountAdapter by lazy {
+        QuestionInfoLikeReplyCountAdapter {
+            viewModel.executeLike(it)
+        }
+    }
+    private lateinit var concatAdapter: ConcatAdapter
     private val replyAdapter: ReplyAdapter by lazy {
         ReplyAdapter()
     }
 
     override fun initView() {
         questionId = intent.getLongExtra("questionId", -1L)
-        Log.d("QuestionInfoActivity", "initView: $questionId")
         if (questionId == -1L) {
             showToast("일시적인 오류가 발생하였습니다. 다시 시도해주세요.")
             return
         }
-
         setupToolbar("")
+
+        concatAdapter = ConcatAdapter(
+            questionInfoContentAdapter,
+            questionInfoImageAdapter,
+            questionLikeReplyCountAdapter,
+            replyAdapter
+        )
+
         viewModel.getQuestion(questionId)
 
         observeQuestionContent()
         observePostReplyState()
         getReplies(questionId)
 
-        binding.replyRv.apply {
+        binding.questionInfoRv.apply {
             itemAnimator = null
-            adapter = replyAdapter
+            adapter = concatAdapter
             layoutManager =
                 LinearLayoutManager(this@QuestionInfoActivity, LinearLayoutManager.VERTICAL, false)
-        }
-
-        binding.likeLayout.setOnSingleClickListener {
-            Log.d(
-                "QuestionInfoActivity",
-                "initView: ${questionId}  ${binding.questionContent!!.isLike}"
-            )
-            viewModel.executeLike(questionId, binding.questionContent!!.isLike)
         }
 
         binding.inputReply.addTextChangedListener(replyTextWatcher)
@@ -80,9 +90,12 @@ class QuestionInfoActivity : BaseActivity<ActivityQuestionInfoBinding>() {
                         is UiState.Loading -> supportFragmentManager.showLoading()
                         is UiState.Success -> {
                             supportFragmentManager.hideLoading()
-                            binding.questionContent = state.data
+                            questionInfoContentAdapter.setContent(state.data.questionContent)
+                            questionLikeReplyCountAdapter.setContent(state.data.questionContent)
+                            questionInfoImageAdapter.submitList(state.data.questionImageUrls)
                         }
                         is UiState.Error -> {
+                            Log.d("QuestionInfo", "observeQuestionContent: ${state.error!!.message} ")
                             supportFragmentManager.hideLoading()
                             state.error!!.message?.let { showToast(it) }
                         }
