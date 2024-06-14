30821ca34db173813ce7250d66f620b26fd4f5b4
MinhoiKoo <minlove2013@gmail.com>
2024-06-14 16:54:56 +0900

    refactor: 이미지 객체로 수정

--- app/src/main/java/com/minhoi/memento/data/dto/question/QuestionResponse.kt
+++ app/src/main/java/com/minhoi/memento/data/dto/question/QuestionResponse.kt
@@ -4,5 +4,5 @@ import com.google.gson.annotations.SerializedName
 
 data class QuestionResponse(
     @SerializedName("subBoardDTO") val questionContent: QuestionContent,
-    @SerializedName("subBoardImageUrls") val questionImageUrls: List<String>
+    @SerializedName("subBoardImageUrls") val questionImageUrls: List<QuestionImage>
 )
