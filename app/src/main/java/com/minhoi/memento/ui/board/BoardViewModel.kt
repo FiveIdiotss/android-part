9e72a12081e59ac39167bf5ff609697aec901242
MinhoiKoo <minlove2013@gmail.com>
2024-06-17 17:21:30 +0900

    refactor: viewModel에서 게시글 올리는 상태 관리하도록 변경 및 이미지 업로드 추가

--- app/src/main/java/com/minhoi/memento/ui/board/BoardViewModel.kt
+++ app/src/main/java/com/minhoi/memento/ui/board/BoardViewModel.kt
@@ -1,17 +1,23 @@
 package com.minhoi.memento.ui.board
 
+import android.net.Uri
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.gson.Gson
 import com.minhoi.memento.MentoApplication
 import com.minhoi.memento.data.dto.BoardContentDto
 import com.minhoi.memento.data.dto.BoardContentResponse
+import com.minhoi.memento.data.dto.MentorBoardPostDto
 import com.minhoi.memento.data.dto.MentoringApplyRequest
+import com.minhoi.memento.data.dto.TimeTableDto
 import com.minhoi.memento.data.model.DayOfWeek
 import com.minhoi.memento.repository.board.BoardRepository
 import com.minhoi.memento.repository.member.MemberRepository
 import com.minhoi.memento.ui.UiState
+import com.minhoi.memento.utils.FileManager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -19,6 +25,8 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.toRequestBody
 import java.time.LocalDate
 import javax.inject.Inject
 
@@ -26,6 +34,7 @@ import javax.inject.Inject
 class BoardViewModel @Inject constructor(
     private val boardRepository: BoardRepository,
     private val memberRepository: MemberRepository,
+    private val fileManager: FileManager
 ) : ViewModel() {
 
     private val member = MentoApplication.memberPrefs.getMemberPrefs()
@@ -47,6 +56,33 @@ class BoardViewModel @Inject constructor(
 
     private val _isMyPost = MutableStateFlow<Boolean>(false)
     val isMyPost: StateFlow<Boolean> = _isMyPost.asStateFlow()
+
+    private val _postImages = MutableStateFlow<List<Uri>>(emptyList())
+    val postImages: StateFlow<List<Uri>> = _postImages.asStateFlow()
+
+    private val _postTitle = MutableStateFlow("")
+    val postTitle: StateFlow<String> = _postTitle
+
+    private val _postIntroduce = MutableStateFlow("")
+    val postIntroduce: StateFlow<String> = _postIntroduce
+
+    private val _postTarget = MutableStateFlow("")
+    val postTarget: StateFlow<String> = _postTarget
+
+    private val _postDescription = MutableStateFlow("")
+    val postDescription: StateFlow<String> = _postDescription
+
+    private val _postConsultTime = MutableStateFlow<Int>(0)
+    val postConsultTime: StateFlow<Int> = _postConsultTime
+
+    private val _postCategory = MutableStateFlow("")
+    val postCategory: StateFlow<String> = _postCategory
+
+    private val _postTimeTable = MutableStateFlow<List<TimeTableDto>>(emptyList())
+    val postTimeTable: StateFlow<List<TimeTableDto>> = _postTimeTable
+
+    private val _postAvailableDays = MutableStateFlow<List<DayOfWeek>>(emptyList())
+    val postAvailableDays: StateFlow<List<DayOfWeek>> = _postAvailableDays
 
     var selectedDate: String = ""
         private set
@@ -108,11 +144,99 @@ class BoardViewModel @Inject constructor(
         }
     }
 
+    fun executeBookmark(boardId: Long, isBookmarked: Boolean) {
+        viewModelScope.launch {
+            when (isBookmarked) {
+                true -> boardRepository.executeUnBookmark(boardId)
+                false -> boardRepository.executeBookmark(boardId)
+            }
+        }
+    }
+
     fun setBookmarkState(state: Boolean) {
         _boardBookmarkState.value = state
     }
 
     fun setMentoringMessage(message: String) {
         mentoringApplyMessage = message
+    }
+
+    fun postBoard() {
+        viewModelScope.launch {
+            val post = MentorBoardPostDto(
+                _postTitle.value,
+                _postIntroduce.value,
+                _postTarget.value,
+                _postDescription.value,
+                _postConsultTime.value,
+                _postCategory.value,
+                _postTimeTable.value,
+                _postAvailableDays.value
+            )
+            val postRequestBody = Gson().toJson(post).toRequestBody("application/json".toMediaTypeOrNull())
+            val images =
+                if (_postImages.value.isEmpty()) null
+                else {
+                    _postImages.value.map {
+                        val fileType = fileManager.getFileMimeType(it)
+                        fileManager.uriToMultipartBodyPart(it, fileType!!, "files")!!
+                    }
+                }
+            Log.d(TAG, "postBoard: $post")
+            boardRepository.postBoard(postRequestBody, images).collect {
+                it.handleResponse(
+                    onSuccess = {
+                        Log.d(TAG, "deleteBoardPost: success")
+                    },
+                    onError = {
+                        Log.d(TAG, "deleteBoardPost: ${it.exception!!.message}")
+                    }
+                )
+            }
+        }
+    }
+
+    fun setTitle(value: CharSequence) { _postTitle.value = value.toString() }
+    fun setIntroduce(value: CharSequence) { _postIntroduce.value = value.toString() }
+    fun setTarget(value: CharSequence) { _postTarget.value = value.toString() }
+    fun setDescription(value: CharSequence) { _postDescription.value = value.toString() }
+    fun setConsultTime(value: Int) { _postConsultTime.value = value }
+    fun setCategory(value: CharSequence) { _postCategory.value = value.toString() }
+    fun setMentorTimeTableList(value: TimeTableDto) {
+        _postTimeTable.value += value
+    }
+    fun deleteMentorTimeTableAt(position: Int) {
+        val currentTimetable = _postTimeTable.value.toMutableList()
+        currentTimetable.removeAt(position)
+        _postTimeTable.value = currentTimetable
+    }
+
+    fun changeConsultTime() {
+        _postTimeTable.value = emptyList()
+    }
+
+    fun setSelectedCheckBoxes(day: DayOfWeek, isChecked: Boolean) {
+        val currentList = _postAvailableDays.value.toMutableList()
+        if (isChecked) {
+            if (!currentList.contains(day)) {
+                currentList.add(day)
+            }
+        } else {
+            currentList.remove(day)
+        }
+        _postAvailableDays.value = currentList
+    }
+
+    fun addPostImages(image: List<Uri>) {
+        _postImages.update { _postImages.value + image }
+    }
+
+    fun removePostImageAt(position: Int) {
+        _postImages.update { postImages ->
+            postImages.toMutableList().apply { removeAt(position) }
+        }
+    }
+    companion object {
+        private const val TAG = "BoardViewModel"
     }
 }
--- app/src/main/java/com/minhoi/memento/ui/board/MentorPostActivity.kt
+++ app/src/main/java/com/minhoi/memento/ui/board/MentorPostActivity.kt
@@ -1,32 +1,43 @@
 package com.minhoi.memento.ui.board
 
-import android.util.Log
-import androidx.lifecycle.lifecycleScope
+import android.annotation.SuppressLint
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.minhoi.memento.R
 import com.minhoi.memento.base.BaseActivity
-import com.minhoi.memento.data.dto.MentorBoardPostDto
-import com.minhoi.memento.data.dto.TimeTableDto
 import com.minhoi.memento.data.model.DayOfWeek
-import com.minhoi.memento.data.network.RetrofitClient
-import com.minhoi.memento.data.network.service.BoardService
 import com.minhoi.memento.databinding.ActivityMentorPostBinding
 import com.minhoi.memento.ui.SelectBoardCategoryBottomSheetDialog
 import com.minhoi.memento.ui.StartEndTimePickerDialog
+import com.minhoi.memento.ui.adapter.AddPhotoAdapter
 import com.minhoi.memento.ui.adapter.MentorTimeTableAdapter
+import com.minhoi.memento.utils.repeatOnStarted
 import com.minhoi.memento.utils.setOnSingleClickListener
 import com.minhoi.memento.utils.showToast
-import kotlinx.coroutines.launch
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MentorPostActivity : BaseActivity<ActivityMentorPostBinding>() {
     override val layoutResourceId: Int = R.layout.activity_mentor_post
-    private lateinit var mentorTimeTableAdapter: MentorTimeTableAdapter
-    private val selectedCheckBoxes = mutableSetOf<DayOfWeek>()
-    private var consultTime: Int = 0
-    private val retrofitClient = RetrofitClient.getLoggedInInstance().create(BoardService::class.java)
-    private val timetables = mutableListOf<TimeTableDto>()
+    private val viewModel by viewModels<BoardViewModel>()
+    private val mentorTimeTableAdapter: MentorTimeTableAdapter by lazy {
+        MentorTimeTableAdapter {
+            // onDeleteClickListener
+            viewModel.deleteMentorTimeTableAt(it)
+        }
+    }
+    private val addPhotoAdapter: AddPhotoAdapter by lazy {
+        AddPhotoAdapter(onDeleteClickListener = {
+            viewModel.removePostImageAt(it)
+        })
+    }
 
     override fun initView() {
+        binding.viewModel = viewModel
+
         val checkBoxes = listOf(
             binding.mondayCheckBox,
             binding.tuesdayCheckBox,
@@ -37,10 +48,27 @@ class MentorPostActivity : BaseActivity<ActivityMentorPostBinding>() {
             binding.sundayCheckBox
         )
 
-        mentorTimeTableAdapter = MentorTimeTableAdapter {
-            // onDeleteClickListener
-            timetables.removeAt(it)
-            mentorTimeTableAdapter.submitList(timetables.toList())
+        checkBoxes.forEachIndexed { index, checkBox ->
+            checkBox.setOnCheckedChangeListener { _, isChecked ->
+                val dayOfWeek = DayOfWeek.values()[index]
+                viewModel.setSelectedCheckBoxes(dayOfWeek, isChecked)
+            }
+        }
+
+        val pickImageContract =
+            registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(10)) { uris ->
+                if (uris.isNotEmpty() && uris != null) {
+                    viewModel.addPostImages(uris)
+                }
+            }
+
+        binding.imageRv.apply {
+            adapter = addPhotoAdapter
+            layoutManager = LinearLayoutManager(this@MentorPostActivity, LinearLayoutManager.HORIZONTAL, false)
+        }
+
+        binding.closeBtn.setOnSingleClickListener {
+            finish()
         }
 
         binding.selectTimeTableLayout.setOnSingleClickListener {
@@ -57,66 +85,43 @@ class MentorPostActivity : BaseActivity<ActivityMentorPostBinding>() {
                 selectCategory(object :
                     SelectBoardCategoryBottomSheetDialog.OnCategorySelectedListener {
                     override fun onCategorySelected(category: String) {
+                        viewModel.setCategory(category)
                         binding.mentoringCategory.text = category
                     }
                 })
             }.show(supportFragmentManager, "selectCategoryFragment")
         }
 
+        binding.addImageBtn.setOnSingleClickListener {
+            pickImageContract.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+
         binding.postButton.setOnSingleClickListener {
             // 게시물 등록
-            selectedCheckBoxes.clear()
-
-            for ((index, checkBox) in checkBoxes.withIndex()) {
-                if (checkBox.isChecked) {
-                    val dayOfWeek = DayOfWeek.values()[index]
-                    selectedCheckBoxes.add(dayOfWeek)
-                }
-            }
-
-            if (selectedCheckBoxes.isEmpty()) {
+            if (viewModel.postAvailableDays.value.isEmpty()) {
                 showToast("요일을 선택해주세요")
                 return@setOnSingleClickListener
             }
 
-            if (consultTime == 0) {
+            if (viewModel.postConsultTime.value == 0) {
                 showToast("상담 시간을 선택해주세요")
                 return@setOnSingleClickListener
             }
 
-            if (timetables.isEmpty()) {
+            if (viewModel.postTimeTable.value.isEmpty()) {
                 showToast("상담 가능한 시간대를 추가해주세요")
                 return@setOnSingleClickListener
             }
 
-            if (binding.mentoringCategory.text == "카테고리 선택") {
+            if (viewModel.postCategory.value.isEmpty()) {
                 showToast("멘토링 대상 단과대를 선택해주세요")
                 return@setOnSingleClickListener
             }
-
-            val post = MentorBoardPostDto(
-                binding.inputTitle.text.toString(),
-                binding.inputIntroduce.text.toString(),
-                binding.inputTarget.text.toString(),
-                binding.inputDescription.text.toString(),
-                consultTime,
-                binding.mentoringCategory.text.toString(),
-                mentorTimeTableAdapter.currentList,
-                selectedCheckBoxes.toList()
-            )
-
-            lifecycleScope.launch {
-                val response = retrofitClient.writeMenteeBoard(post)
-                if (response.isSuccessful) {
-                    showToast("게시물이 등록되었습니다")
-                    finish()
-                } else {
-                    showToast("일시적인 오류가 발생하였습니다. 다시 시도해주세요")
-                    Log.d("TIMETALBEERROR", "initView: ${response.errorBody()}")
-                }
-            }
+            viewModel.postBoard()
         }
         deleteTimeTableWhenTimeSelected()
+        observeTimeTable()
+        observePostImages()
     }
 
     // 30분/1시간 안누르면 시간 추가 못하게
@@ -125,11 +130,11 @@ class MentorPostActivity : BaseActivity<ActivityMentorPostBinding>() {
         val selectedTimeId = binding.radioGroup.checkedRadioButtonId
         when (selectedTimeId) {
             binding.minutesRadioButton.id -> {
-                consultTime = THIRTY_MINUTES
+                viewModel.setConsultTime(THIRTY_MINUTES)
                 showTimePickerDialog(THIRTY_MINUTES)
             }
             binding.hourRadioButton.id -> {
-                consultTime = ONE_HOUR
+                viewModel.setConsultTime(ONE_HOUR)
                 showTimePickerDialog(ONE_HOUR)
             }
             else -> showToast("시간을 선택해주세요")
@@ -141,20 +146,41 @@ class MentorPostActivity : BaseActivity<ActivityMentorPostBinding>() {
         binding.radioGroup.setOnCheckedChangeListener { group, checkedId ->
             when (checkedId) {
                 R.id.minutesRadioButton, R.id.hourRadioButton -> {
-                    timetables.clear()
-                    mentorTimeTableAdapter.submitList(timetables.toList())
+                    viewModel.changeConsultTime()
                 }
             }
         }
     }
 
     private fun showTimePickerDialog(interval: Int) {
-        val timePickerDialog = StartEndTimePickerDialog(interval, timetables) {
-            timetables.add(it)
-            // submitList는 list의 주소가 같으면 변경사항이 없다고 판단하기 때문에 새로운 List 넘기기
-            mentorTimeTableAdapter.submitList(ArrayList(timetables))
+        val timePickerDialog = StartEndTimePickerDialog(interval, viewModel.postTimeTable.value) {
+            viewModel.setMentorTimeTableList(it)
         }
         timePickerDialog.show(supportFragmentManager, "StartEndTimePickerDialog")
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun observePostImages() {
+        repeatOnStarted {
+            viewModel.postImages.collect { images ->
+                binding.imageCount.text = "${images.count()}/10"
+                addPhotoAdapter.submitList(images.map { it.toString() })
+            }
+        }
+    }
+
+    private fun observeTimeTable() {
+        repeatOnStarted {
+            viewModel.postTimeTable.collect {
+                mentorTimeTableAdapter.submitList(it)
+            }
+        }
+    }
+
+    private fun observePostEvent() {
+        repeatOnStarted {
+
+        }
     }
 
     companion object {
